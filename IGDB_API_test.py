ac0b82fb8920f0396d3484f97451381291a3a83e
natebrown-design <natebrown@college.harvard.edu>
2026-04-24 17:45:15 +0000

    Fri, Apr 24, 2026, 1:45 PM -04:00

--- IGDB_API_test.py
+++ IGDB_API_test.py
@@ -1,4 +1,5 @@
 import requests
+import requests_cache
 
 url = "https://id.twitch.tv/oauth2/token"
 
@@ -9,6 +10,22 @@ params = {
 }
 
 response = requests.post(url, params=params)
+
+data = response.json()
+print(data)
+
+# Enable cache (stored in SQLite file)
+requests_cache.install_cache('igdb_cache', expire_after=86400)
+
+url = "https://api.igdb.com/v4/games"
+headers = {
+    "Client-ID": "qnanvz0eh3tpkl5o34ts4yhvhf20rb",
+    "Authorization": f"Bearer {data['access_token']}"
+}
+
+query = "fields name, rating; limit 10;"
+
+response = requests.post(url, headers=headers, data=query)
 
 data = response.json()
 print(data)
