e30ca13ce7c11ff5b6b590222d7d14a820fb9aec
natebrown-design <natebrown@college.harvard.edu>
2026-04-30 21:50:38 +0000

    Thu, Apr 30, 2026, 5:50 PM -04:00

--- fp_distinguish.py
+++ fp_distinguish.py
@@ -201,7 +201,7 @@ def play_game():
                 if guess_lower in used_games:
                     print("❗ You've already used that game! Try a different one.\n")
 
-                if is_valid_guess(guess, i, j):
+                elif is_valid_guess(guess, i, j):
                     board[i][j] = guess
                     used_games.add(guess_lower)
                     score += 1 # modifying a global variable in function, so must declare it at start of play_game
