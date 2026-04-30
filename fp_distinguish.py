ac785d65bcdeee7d243e7f4eac2cc3d37a37d2ad
natebrown-design <natebrown@college.harvard.edu>
2026-04-30 02:00:58 +0000

    Wed, Apr 29, 2026, 10:00 PM -04:00

--- fp_distinguish.py
+++ fp_distinguish.py
@@ -138,7 +138,7 @@ def is_game_in_database(game_name):
     return len(r.json()) > 0
 
 # --- QUERIES THE PLAYER TO WHAT GAME THEY ARE REFERRING TO FOR DISAMBIGUATION (like get_person_id function in pset 5!) ---
-
+def get_game_name()
 
 
 
@@ -182,6 +182,7 @@ def play_game():
                 print(f"Cell: Row = '{row_tags[i]}', Column = '{col_tags[j]}'")
 
                 guess = input("Your guess: ").strip()
+                guess = get
 
                 if is_valid_guess(guess, i, j):
                     board[i][j] = guess
